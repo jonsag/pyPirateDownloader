87b3260a06d7696ab13bbc41a364b6e2d21b91ab
jonsag <jonsagebrand@gmail.com>
2014-07-11 22:00:16 +0200

    First test OK

--- jsPirate.py
+++ jsPirate.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 # Encoding: UTF-8
 
-import ConfigParser, os
+import ConfigParser, os, getopt
 
 config = ConfigParser.ConfigParser()
 config.read("%s/config.ini" % os.path.dirname(__file__)) # read config file
 
-apiBaseUrl = int(config.get('pirateplay','apiBaseUrl')) # base url for pirateplay.se api
-getStreams = int(config.get('pirateplay','getStreams')) # get streams from pirateplay.se
+apiBaseUrl = config.get('pirateplay','apiBaseUrl') # base url for pirateplay.se api
+getStreams = config.get('pirateplay','getStreams') # get streams from pirateplay.se
 
 print "%s/%s" % (apiBaseUrl, getStreams)
